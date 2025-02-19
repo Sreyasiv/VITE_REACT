b4c8417fa99f525613b6cb76597cf281f190006c
Sreya Sivakumar <sreyas@Sreyas-MacBook-Air.local>
2025-02-19 09:16:33 +0530

    updated

---
+++ my-app/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+const App = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Sreya Sivakumar</h1>
+      <p>Contact: sreyasvkmr@gmail.com</p>
+    </div>
+  );
+};
+
+export default App;
